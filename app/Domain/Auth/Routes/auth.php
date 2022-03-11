89fa443a38017803a3bb09567b92b488f63db599
Sean <sstwunett@hotmail.com>
2022-03-11 09:56:36 +0100

    ui to vue name change

---
+++ app/Domain/Auth/Routes/auth.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types = 1);
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Auth Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get();
