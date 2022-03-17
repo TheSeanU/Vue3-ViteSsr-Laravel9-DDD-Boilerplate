681eb7835f24e788c933a7b7610e40b1a933d55a
Sean <sstwunett@hotmail.com>
2022-03-17 16:30:48 +0100

    Update auth.php

--- app/Domain/Auth/Routes/auth.php
+++ app/Domain/Auth/Routes/auth.php
@@ -3,8 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Core\Providers\RouteServiceProvider;
-
 use App\Domain\Auth\Controllers\Authcontroller;
 
 /*
@@ -12,10 +10,5 @@ use App\Domain\Auth\Controllers\Authcontroller;
 | Auth Routes
 |--------------------------------------------------------------------------
 */
-
-
-RouteServiceProvider::addRouteService('api', '',  'App\Domain\Auth\Controllers', 'App\Domain\Auth\Routes\auth.php');
-
-
 
 Route::get('test', [Authcontroller::class, 'test']);
