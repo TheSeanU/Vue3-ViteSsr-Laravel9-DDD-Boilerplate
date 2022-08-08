961eb98787d48d3455773e31c3245150660b26b5
Sean Unett <sstwunett@hotmail.com>
2022-08-08 10:52:11 +0200

    Packages updates, namespace fix & remove webroutes

--- app/Infrastructure/Route/web.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types = 1);
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/{any}', function () {
-    return view();
-    // return file_get_contents(base_path('src\index.html'));
-})->where('any', '.*');
